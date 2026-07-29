d6043bcde6a9e82930a5a03feeb0730676e68a2d
Felix-Virabyan <feliqs.virabyan@tumo.org>
2026-07-29 14:04:53 +0400

    css update js new buttons

--- js/script.js
+++ js/script.js
@@ -30,20 +30,20 @@ const OPENAI_KEY = ''
 const OPENAI_MODEL = ''
 
 
-showBtn.addEventListener('click', function(){
+showBtn.addEventListener('click', function () {
     popup.classList.add('show')
 })
 
-popup.addEventListener('click', function(){
+popup.addEventListener('click', function () {
     popup.classList.remove('show')
 })
 
-const generalMusic = new Audio ('./music/end-sound.mp3')
-const questionSong = new Audio ('./music/questions-sound.mp3')
+const generalMusic = new Audio('./music/end-sound.mp3')
+const questionSong = new Audio('./music/questions-sound.mp3')
 let count = 0
 
 
-let fixed1 = new Audio ('./music/8,000-question.mp3')
+let fixed1 = new Audio('./music/8,000-question.mp3')
 let incorrectSoundFlag = false
 
 generalMusic.loop = true
@@ -51,12 +51,119 @@ generalMusic.loop = true
 
 window.addEventListener('click', () => {
     generalMusic.play()
-},{once: true})
+}, { once: true })
 
 endBtn.addEventListener('click', () => {
-    setTimeout (() => {
+    setTimeout(() => {
         game.style.backgroundImage = ""
-    },2000)
+    }, 2000)
     questionSong.pause()
     mainGame.classList.remove('animate__backInUp')
-})
+    gameWrapper.classList.remove('animate__flipInx')
+    mainGame.classList.add('animate__animated', 'animate__backOutDown')
+    setTimeout(() => {
+        mainGame.style.display = 'none'
+        startBtn.style.display = 'block'
+        startBtn.classList.remove('animate__backOutUp')
+        startBtn.classList.add('animate__backInDown')
+
+    }, 1000);
+    setTimeout(() => {
+        startBtn.classList.remove('animate__backInDown')
+
+
+    }, 2000)
+
+    let userWin = document.querySelector('.user-win')
+    if (userWin) {
+        userWin.remove()
+
+    }
+    fixed1.pause()
+    generalMusic.pause()
+
+
+    let activeWin = document.querySelector('.wins-active') || document.querySelector('.win-guaranteed');
+    if (activeWin) {
+        let span = activeWin.querySelectorAll('span')
+        span.forEach(span => span.remove());
+        let visibleAmount = activeWin.innerText.trim()
+        let existingWin = document.querySelector('.user-win')
+        if (existingWin) {
+            existingWin.remove()
+
+        }
+
+    }
+
+    let winDiv = document.createElement('div')
+    winDiv.className = '.user-win animate__animated animate__fadeIn'
+    winDiv.style.cssText = 'text-align:center; font-size: 24px; color: white ; margin-top: 300px; ';
+    startBtn.insertAdjacentElement('afterend ,winDiv')
+
+    setTimeout(()=>{
+        winDiv.classList.replace('animate__fadeIn', 'animate__fadeOut')
+        setTimeout(()=>winDiv.remove(),2000)
+
+
+    },0)
+
+    getStartGame()
+
+
+
+
+
+
+});
+
+
+
+startBtn.addEventListener('click', () => {//Խաղի սկիզբը կոճակի վրա սեղմելիս , պետք է կատարվեն այս ֆունկցիայում ներառված գործողությւոնները
+    generalMusic.pause();
+    generalMusic.currentTime = 0;
+    game.style.backgroundImage = "url('./img/galaxy.jpg')";
+    game.style.backgroundSize="100%"
+  
+    startBtn.classList.add('animate__animated', 'animate__backOutUp');//նախապես ունեցած կոճակի վրա ավելացնում ենք այս երկու անիմացիաները
+    mainGame.classList.remove('animate__backOutDown');//mainGame-ից հեռացնում ենք այս կլաս անուն ունեցող անիմացիան
+    showBtn.remove()
+    setTimeout(() => {//Ցույց է տալիս թե ինչքան ժամանակ հետո պետք է կատարվի տվյալ գործողությունը
+      mainGame.style.display = 'block';
+      mainGame.classList.add('animate__animated', 'animate__backInUp');//mainGame-ին ավելացնումէ է նախապես ստեղծված  կլաս անվանում
+      startBtn.style.display = 'none';
+      setTimeout(() => {
+        gameWrapper.classList.add('animate__animated', 'animate__flipInX');//gameWrapper-ին ավելացնումէ է նախապես ստեղծված  կլաս անվանում
+      }, 1000);
+    }, 500);
+    setTimeout(() => {
+      endBtn.style.opacity = '1';// տրված է առավելագույն թափանցելիություն
+    }, 1000);
+    //
+    setTimeout(() => {
+      questionSong.loop = true
+      questionSong.play()
+      for (let i = 0; i < btnAnswers.length; i++) {
+        btnAnswers[i].addEventListener('click', () => {
+          questionSong.pause()
+          setTimeout(() => {
+            if (count != 5 && count != 10 && count != 15) {
+              if (incorrectSoundFlag == false && count < 5) {
+                questionSong.play()
+              }
+              if (count == 15) {
+                fixed1.pause()
+              }
+              questionSong.currentTime = 0
+            } else if (count >= 5) {
+              fixed1.loop = true
+              fixed1.play()
+              questionSong.pause()
+            }
+          }, 3000);
+  
+        })
+      }
+    }, 2000);
+  });
+
